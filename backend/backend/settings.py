e1c4209cee70bb11cf52f79d7b5948af8456eafc
annabellaals <bebe_chanel_6@hotmail.com>
2024-11-15 00:05:50 +0500

    Added mode for fixing python debug mode

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y@xi6v7sq-6-4b%!u#p0unb5lxde0l3rwa#%ak(=gyn&fzmv+x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-    'wheelshub',
+    'wheelsHub',
 ]
 
 MIDDLEWARE = [
